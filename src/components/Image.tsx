23b6a6a9862b8924027574aedacd10c5f6664acf
jonghunyang <jhaugusty@gmail.com>
2019-08-25 19:05:02 +0900

    apply Typescript
    - remove prop-types
    - gatsby-plugin-typescript
    - typescript
    - @types/react
    - @types/react-dom
    - @types/react-helmet
    - @types/node

--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -1,6 +1,6 @@
-import React from "react"
-import { useStaticQuery, graphql } from "gatsby"
-import Img from "gatsby-image"
+import React from 'react';
+import { useStaticQuery, graphql } from 'gatsby';
+import Img from 'gatsby-image';
 
 /*
  * This component is built using `gatsby-image` to automatically serve optimized
@@ -13,20 +13,21 @@ import Img from "gatsby-image"
  * - `useStaticQuery`: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
-const Image = () => {
-  const data = useStaticQuery(graphql`
-    query {
-      placeholderImage: file(relativePath: { eq: "gatsby-astronaut.png" }) {
-        childImageSharp {
-          fluid(maxWidth: 300) {
-            ...GatsbyImageSharpFluid
-          }
+const LOAD_PLACEHOLDER_IMG = graphql`
+  query {
+    placeholderImage: file(relativePath: { eq: "gatsby-astronaut.png" }) {
+      childImageSharp {
+        fluid(maxWidth: 300) {
+          ...GatsbyImageSharpFluid
         }
       }
     }
-  `)
+  }
+`;
 
-  return <Img fluid={data.placeholderImage.childImageSharp.fluid} />
-}
+const Image = () => {
+  const data = useStaticQuery(LOAD_PLACEHOLDER_IMG);
+  return <Img fluid={data.placeholderImage.childImageSharp.fluid} />;
+};
 
-export default Image
+export default Image;
---
+++ src/components/SEO.tsx
@@ -0,0 +1,77 @@
+import React from 'react';
+import Helmet from 'react-helmet';
+import { useStaticQuery, graphql } from 'gatsby';
+
+const GET_META_DATA = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+        description
+        author
+      }
+    }
+  }
+`;
+
+type MetaProps = React.DetailedHTMLProps<React.MetaHTMLAttributes<HTMLMetaElement>, HTMLMetaElement>;
+
+interface SeoProps {
+  description?: string;
+  lang?: string;
+  meta?: MetaProps[];
+  title: string;
+}
+
+const SEO: React.FC<SeoProps> = ({ description = '', lang = 'en', meta = [], title }) => {
+  const { site } = useStaticQuery(GET_META_DATA);
+
+  const metaDescription = description || site.siteMetadata.description;
+
+  return (
+    <Helmet
+      htmlAttributes={{
+        lang,
+      }}
+      title={title}
+      titleTemplate={`%s | ${site.siteMetadata.title}`}
+      meta={[
+        {
+          name: `description`,
+          content: metaDescription,
+        },
+        {
+          property: `og:title`,
+          content: title,
+        },
+        {
+          property: `og:description`,
+          content: metaDescription,
+        },
+        {
+          property: `og:type`,
+          content: `website`,
+        },
+        {
+          name: `twitter:card`,
+          content: `summary`,
+        },
+        {
+          name: `twitter:creator`,
+          content: site.siteMetadata.author,
+        },
+        {
+          name: `twitter:title`,
+          content: title,
+        },
+        {
+          name: `twitter:description`,
+          content: metaDescription,
+        },
+        ...meta,
+      ]}
+    />
+  );
+};
+
+export default SEO;
