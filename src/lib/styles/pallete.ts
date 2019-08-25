25e44e560dc05d5b7ba17f54e5ee3e30baf90767
jonghunyang <jhaugusty@gmail.com>
2019-08-25 21:11:01 +0900

    add palette color

---
+++ src/lib/styles/pallete.ts
@@ -0,0 +1,18 @@
+/**
+ * https://color.adobe.com/ko/search?q=beautiful%20woman%20in%20a%20colorful%20wig
+ */
+
+const palette = {
+  /* Eastern Blue #248EA6 */
+  eBlue0: '#248EA6',
+  /* Scooter #25C7D9 */
+  gray0: '#25C7D9',
+  /* Golden Dream #F2D338 */
+  gold0: '#fff5f5',
+  /* Orange #F2762E */
+  orange0: '#F2762E',
+  /* Pomegranate #F23030 */
+  Pome0: '#F23030',
+};
+
+export default palette;
